69dd8f45b521b5462e452dd0a5b8bbb90bc81f3b
Konrad Eisele <eiselekd@gmail.com>
2019-06-02 10:34:19 +0200

    all

--- Makefile.debian.mk
+++ Makefile.debian.mk
@@ -4,3 +4,4 @@ git clone https://github.com/eiselekd/pygit2.git
 (cd libgit2; make -f Makefile.debign.mk)
 sudo dpkg -i libgit2-27_0.27.7+dfsg.1-0.1_amd64.deb  libgit2-dev_0.27.7+dfsg.1-0.1_amd64.deb
 (cd libgit2; python setup.py install --user)
+(cd libgit2; python3 setup.py install --user)
